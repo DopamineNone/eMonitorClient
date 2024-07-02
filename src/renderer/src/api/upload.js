3c94ca6bf6199a027c7f11c23abeb530a4e8fa44
Fanglinrui <Fanglinrui@bupt.edu.cn>
2024-07-02 20:02:19 +0800

    upload.js finished

--- src/renderer/src/api/upload.js
+++ src/renderer/src/api/upload.js
@@ -2,3 +2,17 @@
  * 上传API处理函数，包括：
  * 1. 上传请求函数
  */
+import { sendRequest } from '../utils/wss.js' // 导入发送请求函数
+
+function uploadRequest(data) {
+    const uploadData = {
+        type: 'screenshot',
+        msg: {
+            timestamp: new Date().toISOString(), // 时间戳
+            screenshot: data //base64格式的图片数据
+        }
+    }
+    sendRequest(uploadData) // 发送请求
+}
+
+export { uploadRequest }

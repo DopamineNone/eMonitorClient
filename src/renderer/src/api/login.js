f400f3d96c116926f941ec2a9f2d01290cf36ae2
Fanglinrui <Fanglinrui@bupt.edu.cn>
2024-07-02 18:59:55 +0800

    auth ver 0.0.1

--- src/renderer/src/api/login.js
+++ src/renderer/src/api/login.js
@@ -3,13 +3,20 @@
  * 1. 登录请求函数
  * 2. 登录响应函数
  */
-
+import { sendRequest } from '../utils/wss.js' // 导入发送请求函数
 function loginRequestHandler(username, password) {
+    const loginData = {
+        type: 'login',
+        username: username,
+        password: password
+    }
 
+    sendRequest(JSON.stringify(loginData)) // 发送登录请求
 }
 
-function loginResponseHandler(reponse) {
-
+function loginResponseHandler(response) {
+    const status = response.status // 获取登录成功后的status
+    return status
 }
 
 export { loginRequestHandler, loginResponseHandler }
--- src/renderer/src/api/register.js
+++ src/renderer/src/api/register.js
@@ -3,13 +3,20 @@
  * 1. 注册请求函数
  * 2. 注册响应函数
  */
+import { sendRequest } from '../utils/wss.js' // 导入发送请求函数
 
 function registerRequestHandler(username, password) {
+    const registerData = {
+        type: 'register',
+        username: username,
+        password: password
+    }
 
+    sendRequest(JSON.stringify(registerData)) // 发送请求
 }
 
 function registerResponseHandler(reponse) {
-
+    // 注册响应处理函数,看看api会怎么更新
 }
 
 export { registerRequestHandler, registerResponseHandler }

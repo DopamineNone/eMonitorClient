8aaf21616dd764b8f6ff1a98d3672aef3d8498d1
Fanglinrui <Fanglinrui@bupt.edu.cn>
2024-07-03 13:03:17 +0800

    get client mac

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,20 +1,36 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+const os = require('os')
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+    getMacAddress: () => {
+        const networkInterfaces = os.networkInterfaces()
+        // 查找第一个非内部的网络接口的 MAC 地址
+        for (let key in networkInterfaces) {
+            const iface = networkInterfaces[key]
+            for (let i = 0; i < iface.length; i++) {
+                const { address, internal } = iface[i]
+                if (!internal && address && address !== '127.0.0.1' && address !== '::1') {
+                    return iface[i].mac.toUpperCase()
+                }
+            }
+        }
+        return null // 如果找不到有效的 MAC 地址，返回 null
+    }
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        contextBridge.exposeInMainWorld('electron', electronAPI)
+        contextBridge.exposeInMainWorld('api', api)
+    } catch (error) {
+        console.error(error)
+    }
 } else {
-  window.electron = electronAPI
-  window.api = api
+    window.electron = electronAPI
+    window.api = api
 }
--- src/renderer/src/api/login.js
+++ src/renderer/src/api/login.js
@@ -6,10 +6,12 @@
 import { sendRequest } from '../utils/wss.js' // 导入发送请求函数
 import { useStatusStore } from '../store/status.js'
 function loginRequestHandler(username, password) {
+    const mac = window.api.getMacAddress() // 获取mac地址
     const loginData = {
         type: 'login',
         username: username,
-        password: password
+        password: password,
+        mac: mac
     }
 
     sendRequest(JSON.stringify(loginData)) // 发送登录请求

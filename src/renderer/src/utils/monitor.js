7749dac0ca28e2323b89f07ed631b884bfafd5ab
Fanglinrui <Fanglinrui@bupt.edu.cn>
2024-07-03 17:02:14 +0800

    compress and base64 finished

--- src/renderer/src/utils/monitor.js
+++ src/renderer/src/utils/monitor.js
@@ -4,6 +4,7 @@
  * 2. 截屏处理函数
  */
 import { remote } from 'electron'
+const JSZip = require('jszip')
 
 // 获取屏幕监控视频流
 export function getScreenStream() {
@@ -73,4 +74,29 @@ export function getScreenShot(videoDOM) {
 
     // 将canvas转换为图片(base64)
     return canvas.toDataURL('image/png')
+}
+
+//使用了上面的getScreenshot函数
+export function shot2zipONbase64(videoDOM) {
+    // 创建一个新的 JSZip 实例
+    const zip = new JSZip()
+
+    // 获取截图的 base64 编码图像数据,以及时间戳
+    let screenshotData = getScreenShot(videoDOM)
+    const timestamp = Date.now()
+    const filename = `screenshot_${timestamp}.png`
+
+    // 将 base64 图像数据添加到 ZIP 文件中，使用时间戳命名文件
+    zip.file(filename, screenshotData.split('base64,')[1], { base64: true })
+
+    // 生成 ZIP 文件
+    return zip
+        .generateAsync({ type: 'base64' })
+        .then(function (base64) {
+            return base64
+        })
+        .catch(function (err) {
+            console.error('Error creating ZIP file: ', err)
+            return null
+        })
 }

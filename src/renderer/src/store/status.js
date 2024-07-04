7533fe9eaf3f509ef79241ac4d9f77bc0d8cf528
DopamineNone <c20160419raytheon@163.com>
2024-07-04 14:44:58 +0800

    perf store for status

--- src/renderer/src/store/status.js
+++ src/renderer/src/store/status.js
@@ -1,25 +1,29 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 // 与客户端主机有关的信息仓库
 export const useStatusStore = defineStore('status', () => {
     // websocket连接器
-    let wss = null
+    const wss = ref(null)
+
+    // 屏幕视频流
+    const stream = ref(null)
+
+    //上传频率
+    const uploadFrequency = ref(10000)
 
     const isConnected = computed(() => {
-        return wss !== null && wss?.readyState === WebSocket.OPEN
+        return wss.value !== null && wss.value?.readyState === WebSocket.OPEN
     })
-
-    // 监控视频流对象
-    let videoStream = null
 
     // 登录标志位
     const isLoggedIn = ref(false)
 
     return {
         wss,
+        stream,
+        uploadFrequency,
         isConnected,
-        videoStream,
         isLoggedIn
     }
 })
